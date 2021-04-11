7997795ef4e2e81aec68de6c330da58033581872
koki-a <mk.eng1119@gmail.com>
2021-04-11 16:01:09 +0900

    header修正

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -11,10 +11,8 @@
                     <li class="header_item"><a href="{{ route('register') }}" class="header_link">ユーザ登録</a></li>
                     <li class="header_item"><a href="{{ route('login') }}" class="header_link">ログイン</a></li>
                     @else
-                    <li class="header_item"><button form="logout-button" class="logout_button" type="submit">
-                        ログアウト
-                    </button>
-                    </li>
+                    <li class="header_item"><a href="{{ route('shops.create') }}" class="header_link">投稿する</a></li>
+                    <li class="header_item"><button form="logout-button" class="logout_button" type="submit">ログアウト</button></li>
                     <form id="logout-button" method="POST" action="{{ route('logout') }}">
                     @csrf
                     </form>
